2953e00cdaeb511430e6faa30706d21ecdb3c6c0
promisemagoga <tshegofatsopromise362@gmail.com>
2023-10-20 10:57:43 +0200

    Created backend

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const app = express();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+app.use(bodyParser.json())
+
+
+app.post('/payment', async (req, res) => {
+ try {
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: req.body.amount,
+        currency: 'usd',
+        automatic_payment_methods: {
+            enabled: true,
+        }
+    })
+    res.json({paymentIntent: paymentIntent.client_secret})
+ } catch (error) {
+    res.status(400).json({
+        error: error.message,
+    })
+ }
+
+
+})
+
+
+
+const port = 8000
+app.listen(port, (error) => {
+    if(error) throw error
+    console.log(`server running on port`, port)
+})
+
